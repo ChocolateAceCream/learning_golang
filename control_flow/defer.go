d6788abfac832a666b4fa64b532cd00e1b0049ac
ChocolateAceCream <nuodi@hotmail.com>
2021-06-22 15:58:10 +0800

    FEAT: add loop and defer example

---
+++ control_flow/defer.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+/*
+	actual flow of main() function is that
+	fmt.Println("first")
+	then it sees defer, so jump to
+	fmt.Println("third")
+	after execution of all its statement done, right before function return(exit)
+	go will check for any defer statements and call it in Last in first out order .
+*/
+func main() {
+	fmt.Println("first")
+	defer fmt.Println("defer first")
+	fmt.Println("second")
+	defer fmt.Println("defer second")
+	fmt.Println("third")
+	defer fmt.Println("defer third")
+
+	fmt.Println("----variable changed example---------")
+	a := 10
+	//will print a= 10 because the when you defer a function, the args of function are taken at the time defer is called,
+	// not the time defer is executed.
+	defer fmt.Println("a = ? ", a)
+	a = 20
+
+	res, err := http.Get("https://gitlab.ctbiyi.com/smart_manage/smart_important_window_mobile/commits/dev")
+	if err != nil {
+		log.Fatal(err)
+	}
+	//this most common useage of defer: closing http request is preferred so no need to worry about other logic in between
+	//open and close request, since this defer statement will be executed last
+	//p.s. when open resource in loop, better avoid using this pattern
+	defer res.Body.Close()
+	robots, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("%s", robots)
+}
--- loops/For.go
+++ loops/For.go
@@ -5,7 +5,83 @@ import (
 )
 
 func main() {
-	for i := 0; i < 5; i++ {
+	// initialize counter this way, then counter is only accessable inside the for loop scope.
+	for i := 0; i < 5; i = i + 2 {
 		fmt.Println(i)
+	}
+	// counter could also be initialized else where, then it will be accessible outside the for loop
+	//the incrementor can only take statement, and it's not required(you can increment counter insdie loop explicitly)
+	//then the for loop will turn to a while loop
+	i := 0
+	fmt.Println("----i----")
+	for i < 5 {
+		i++
+	}
+	fmt.Println(i)
+
+	//break is used to jump out of the nearest loop(only that loop), in this case, the infinite loop
+	//if that loop is wrapped by other loops, then other loops will continue their iteration
+	for {
+		i = i + 4
+		if i > 100 {
+			break //jump out of the loop
+		}
+	}
+
+	//optional, we can use label Loop:  to tell go where we want to break the loop to
+	fmt.Println("---Loop break example----")
+Loop:
+	for i := 0; i < 5; i++ {
+		// Loop: also can be placed here
+		for j := 0; j < 5; j++ {
+			for k := 0; k < 5; k++ {
+				fmt.Println("---i, j,k----", i, j, k)
+				if k > 3 {
+					break Loop
+				}
+			}
+		}
+	}
+
+	//use continue to skip current iteration
+	fmt.Println("-----continue example---")
+	for i := 0; i < 5; i++ {
+		if i%2 == 0 {
+			continue
+		}
+		fmt.Println(i)
+	}
+
+	fmt.Println("infinite loop", i)
+
+	//looping two variables
+	fmt.Println("looping two variables")
+	for i, j := 1, 2; i < 5; i, j = i+1, j+2 {
+		fmt.Println(i, j)
+	}
+
+	//use for loop to iterate a collection, such as slice, map, string etc..
+	fmt.Println("------collection iteration with for loop------------")
+	slice := []int{111, 222, 333, 444, 555}
+	for i, v := range slice {
+		fmt.Printf("value: %v, index: %v \n", v, i)
+	}
+
+	fmt.Println("------loop through string------------")
+	str := "asdfasdf"
+	for i, v := range str {
+		// value will be returned in form of unicode by default, so we need to cast it to string
+		fmt.Printf("value: %v, index: %v \n", string(v), i)
+	}
+	fmt.Println("------loop through map------------")
+	m := map[string]int{
+		"h": 1,
+		"e": 2,
+	}
+
+	//if only value is required : for _, v := range m
+	//if only key is required : for k := range m
+	for k, v := range m {
+		fmt.Printf("value: %v, key: %v \n", v, k)
 	}
 }
