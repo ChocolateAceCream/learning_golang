07e904eb2ce053c25e630c2592184ed7adafa320
ChocolateAceCream <nuodi@hotmail.com>
2022-01-19 17:27:15 +0800

    FEAT: add slice usage example

--- Arrays_Slice/Slice.go
+++ Arrays_Slice/Slice.go
@@ -11,4 +11,10 @@ func main() {
 	for key, value := range m {
 		fmt.Println(key, "->", value)
 	}
+
+	s1 := []int{1, 2, 3}
+	s2 := []int{2, 3, 4}
+	// cannot use slice directly as second param in append()
+	s1 = append(s1, s2...)
+	fmt.Println(s1)
 }
